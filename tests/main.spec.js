2cbce1b3ee91dcfe209058745cfb5676ee5d5410
Gabriel Givigier <gabriel@givigier.com>
2018-01-23 19:11:14 -0200

    Add --version command to return the version of the CLI

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,15 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
+const exec = require('child_process').exec;
+const pkg = require('../package.json');
 
-describe('Main', () => {
+const btcConverter = './src/main.js';
 
+describe('BTC Converter', () => {
+  it('should return the version of the btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    });
+  });
 });
